6958a1c47fa13e340dd42c75fa42acf280805b9f
Gaby Nieva <gaby.nieva@outlook.com>
2023-02-10 15:40:42 -0600

    Agregando FloatingActionButton

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,25 +1,32 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-   return  Scaffold(
-    appBar: AppBar(
-      title: const Text('HomeScreen'),
-      elevation: 10,
-    ),
-    backgroundColor: Colors.indigo,
-     body: const Center(
-            child:Column(
-                  children: [
-                    Text('Número de taps:'),
-                    Text('0'),
-                  ]
-            ),
-        ),
-   );
+    const TextStyle fontSize30 = TextStyle(fontSize: 30);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeScreen'),
+        elevation: 10,
+      ),
+      backgroundColor: Colors.indigo,
+      body: const Center(
+        child: Column(children: [
+          Text(
+            'Número de taps:',
+            style: fontSize30,
+          ),
+          Text('0'),
+        ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          print('Presionaste el botón');
+        },
+      ),
+    );
   }
-
 }
